7d69b092361d7fddbac8260ab567cd30e616d2ac
Hannah Bialik <hrb44@scarletmail.rutgers.edu>
2024-09-24 14:37:27 -0400

    some a_star

--- A_star.py
+++ A_star.py
@@ -1,4 +1,6 @@
 from env.block import Block
+import heapq
+from collections import deque
 
 class A_star:
 
@@ -8,7 +10,26 @@ class A_star:
 
         return xDiff + yDiff
     
-    def calculateF(self, grid, currPos, target):
+    # def calculateF(self, grid, currPos, target):
+
+    def get_adjacent_nodes(self, current_node):
+        x = current_node.x
+        y = current_node.y
+
+        return 
+
+    def a_star(self, grid, start_node, goal_node):
+        open_list = []
+        closed_list = deque()
+        heapq.heappush(open_list, start_node)
+        
+        while not (not open_list):
+            current_node = heapq.heappop(open_list)
+            if current_node == goal_node:
+                    break
+            closed_list.append(current_node) #whenever claculating g, call updateMaxG for tie breaking
+
+
         
 
         
