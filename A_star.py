cc7e69547b87f84d2a0d52e2b59098541188c91f
Hannah Bialik <hrb44@scarletmail.rutgers.edu>
2024-09-24 15:16:58 -0400

    a_star wip

--- A_star.py
+++ A_star.py
@@ -17,7 +17,7 @@ class A_star:
 
         return xDiff + yDiff
     
-    # def tieBreaker(self, sucessor1, sucessor2):
+    def tieBreaker(self, sucessor1, sucessor2):
         f1, g1 = sucessor1
         f2, g2 = sucessor2
 
@@ -29,22 +29,43 @@ class A_star:
         else:    
             return sucessor2
 
-    def get_adjacent_nodes(self, current_node):
+    def get_adjacent_nodes(self, grid, current_node):
         x = current_node.x
         y = current_node.y
-
-        return 
+        neighbors = deque()
+        if(x-1 > 0):
+            neighbors.append(Block(x-1, y))
+        if(x+1<len(grid) - 1):
+            neighbors.append(Block(x+1, y))
+        if(y-1 > 0):
+            neighbors.append(Block(x, y-1))
+        if(y+1<len(grid) - 1):
+            neighbors.append(Block(x, y-1))
+        return neighbors
 
     def a_star(self, grid, start_node, goal_node):
         open_list = []
         closed_list = deque()
-        heapq.heappush(open_list, start_node)
+        heapq.heappush(open_list, (0, start_node))
         
         while not (not open_list):
             current_node = heapq.heappop(open_list)
             if current_node == goal_node:
                     break
             closed_list.append(current_node) #whenever claculating g, call updateMaxG for tie breaking
+            neighbors = current_node.get_adjacent_nodes(grid)
+            for neighbor in neighbors:
+               if neighbor in closed_list:
+                   continue
+               neighbor.h = neighbor.getManhattanDistance(goal_node)
+               neighbor.g = current_node.g + 1 #this is definately not true but I'm ignoring that for now
+               if neighbor in open_list:
+                   if neighbor.g <= neighbor.f: #this is not the correct comparrison
+                       continue
+                       
+
+
+
 
 
         
--- src/env/block.py
+++ src/env/block.py
@@ -3,6 +3,9 @@ class Block:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.g = 0
+        self.h = 0
+        self.f = self.g + self.h
 
     def __eq__(self, other):
         return isinstance(other, Block) and self.x == other.x and self.y == other.y
