11b27ff48700051a9e82a3d3e2f63dacb0e0317c
noastrupinsky <noa.strupinsky@gmail.com>
2024-09-24 14:35:25 -0400

    helper funcitons  for A* and clean up

--- A_star.py
+++ A_star.py
@@ -1,6 +1,13 @@
 from env.block import Block
 
 class A_star:
+    def __init__(self):
+        self.max_g = 0
+        self.c = 1
+    
+    def updateMaxG(self, curr_g):
+        self.max_g = max(self.max_g, curr_g)
+        self.c = self.max_g + 1
 
     def getManhattanDistance(self, currPos, target):
         xDiff = abs(currPos.x - target.x)
@@ -8,7 +15,17 @@ class A_star:
 
         return xDiff + yDiff
     
-    def calculateF(self, grid, currPos, target):
+    def tieBreaker(self, sucessor1, sucessor2):
+        f1, g1 = sucessor1
+        f2, g2 = sucessor2
+
+        priorityA = (self.c * f1) - g1
+        priorityB = (self.c * f2) - g2
+
+        if priorityA > priorityB:
+            return sucessor1
+        else:    
+            return sucessor2
         
 
         
--- src/env/test.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-# Generate some test data
-data = np.random.rand(10, 10)
-
-# Display the data as an image
-plt.imshow(data, cmap='binary', interpolation='nearest')
-plt.show()
