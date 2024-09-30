9925b2457334cc9bb7f4cc29d0dfdceb2ee7a9f9
Hannah Bialik <hrb44@scarletmail.rutgers.edu>
2024-09-30 09:37:28 -0400

    wip

--- src/env/A_star.py
+++ src/env/A_star.py
@@ -41,7 +41,6 @@ class A_star:
                 TieBreaker().updateMaxG(neighbor.g)
                 neighbor.f = neighbor.g + neighbor_h
 
-
                 if neighbor in open_list:
                     if f_lookup_table[neighbor][0] <= neighbor.f:
                         continue
